640eedcf6c9e89a9ac344350b3ca2038c1e43942
Simone Carletti <weppos@weppos.net>
2016-02-01 17:02:44 +0100

    Add client HTTP implementation

---
+++ README.md
@@ -0,0 +1,69 @@
+# Domainr API client
+
+A Go client for the [Domainr API](http://domainr.build/).
+
+
+## Getting started
+
+```shell
+$ git clone git@github.com:weppos/domainr-go.git
+$ cd domainr-go
+```
+
+or download it as a Go dependency
+
+```shell
+$ go get weppos/domainr-go
+```
+
+Run the test suite.
+
+
+## Testing
+
+```shell
+$ go test ./...
+```
+
+### Live Testing
+
+```shell
+$ export DOMAINR_CLIENT_ID="some-magic-client-id"
+$ go test ./... -v
+```
+
+**Example output**
+
+```shell
+$ go test ./... -v
+=== RUN   TestNewClient
+--- PASS: TestNewClient (0.00s)
+=== RUN   TestLivePrivateGetStatus
+<nil>
+&{[{domainr.com com active active}] 0xc820332b40}
+--- PASS: TestLivePrivateGetStatus (1.13s)
+=== RUN   TestLiveGetStatus
+<nil>
+&{domainr.com com active active}
+--- PASS: TestLiveGetStatus (0.24s)
+PASS
+ok  	github.com/weppos/domainr-go/domainr	1.385s
+```
+
+**Custom domain list**
+
+```shell
+$ DOMAINR_STATUS_DOMAINS=dnsimple.com,domainr.com go test ./... -v
+=== RUN   TestNewClient
+--- PASS: TestNewClient (0.00s)
+=== RUN   TestLivePrivateGetStatus
+<nil>
+&{[{dnsimple.com com active registrar registrar} {domainr.com com active active}] 0xc82041a090}
+--- PASS: TestLivePrivateGetStatus (0.50s)
+=== RUN   TestLiveGetStatus
+<nil>
+&{dnsimple.com com active registrar registrar}
+--- PASS: TestLiveGetStatus (0.26s)
+PASS
+ok  	github.com/weppos/domainr-go/domainr	0.772s
+```
--- domainr/domainr.go
+++ domainr/domainr.go
@@ -4,7 +4,13 @@
 package domainr
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 const (
@@ -36,4 +42,80 @@ type Client struct {
 func NewClient(clientID string) *Client {
 	client := &Client{ClientID: clientID, HttpClient: &http.Client{}}
 	return client
+}
+
+// NewRequest creates an API request.
+// The path is expected to be a relative path and will be resolved according to the BaseURL of the Client.
+// Paths should always be specified without a preceding slash.
+//
+// Domainr only requires GET requests, therefore this method assumes you want GET
+// and also doesn't provide a way to specify a payload.
+func (c *Client) NewRequest(path string) (*http.Request, error) {
+	u, err := url.Parse(baseURL + path)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing request URL: %s", err)
+	}
+
+	// Append the client_id to the query
+	q := u.Query()
+	q.Add("client_id", c.ClientID)
+	u.RawQuery = q.Encode()
+
+	// Build the request
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("User-Agent", userAgent)
+
+	return req, nil
+}
+
+// Do sends an API request and returns the API response.
+func (c *Client) Do(req *http.Request, obj interface{}) (*http.Response, error) {
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	err = c.checkResponse(resp)
+	if err != nil {
+		return resp, err
+	}
+
+	// If obj implements the io.Writer,
+	// the response body is decoded into v.
+	if obj != nil {
+		if w, ok := obj.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(obj)
+		}
+	}
+
+	return resp, err
+}
+
+type errorMessage struct {
+	Message string `json:"message"`
+}
+
+func (c *Client) checkResponse(resp *http.Response) error {
+	// {"message":"unauthorized: invalid API client ID"}
+	switch resp.StatusCode {
+	case 200:
+		return nil
+	default:
+		message := errorMessage{}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err = json.Unmarshal(body, &message); err != nil {
+			return err
+		}
+
+		return errors.New(message.Message)
+	}
 }
